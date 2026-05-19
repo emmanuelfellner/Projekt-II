86823b0ef4c32bad96454aa23376f6e98d113f5d
julia <julia.schubatzky@gmail.com>
2026-05-19 11:31:33 +0200

    Neue Multiplikationsfunktion implementieren #3
    Klasse "Multiplikation" mit Methode "calculate" hinzugefügt

---
+++ Calculator/src/calculator/operations/Multiplikation.java
@@ -0,0 +1,8 @@
+package calculator.operations;
+
+public class Multiplikation {
+    public static double calculate(double a, double b) {
+        System.out.println("Ergebnis: " + a*b);
+        return a * b;
+    }
+}
