2a3c9dac028d60d36e8ae1b9c600c7b47f5d5202
iL-MaKi <maki2672@gmail.com>
2026-05-19 12:08:33 +0200

    Mehrfachverwendung von Calculator
    Einfügen einer while Schleife

--- Calculator/src/calculator/Main.java
+++ Calculator/src/calculator/Main.java
@@ -4,23 +4,39 @@ import calculator.utils.InputHelper;
 
 public class Main {
 
-    static void main() {
+    public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
 
         System.out.println("=== Einfacher Taschenrechner ===");
 
-        double number1 = InputHelper.readDouble("Erste Zahl: ");
-        double number2 = InputHelper.readDouble("Zweite Zahl: ");
+        boolean weiter = true;
 
-        System.out.println("Wähle Operation:");
-        System.out.println("+  - Addition");
-        System.out.println("-  - Subtraktion");
+        while (weiter) {
 
-        String operator = InputHelper.readString("Operator: ");
+            double number1 = InputHelper.readDouble("Erste Zahl: ");
+            double number2 = InputHelper.readDouble("Zweite Zahl: ");
 
-        double result = calculator.calculate(number1, number2, operator);
+            System.out.println("Wähle Operation:");
+            System.out.println("+  - Addition");
+            System.out.println("-  - Subtraktion");
 
-        System.out.println("Ergebnis: " + result);
+            String operator = InputHelper.readString("Operator: ");
+
+            double result = calculator.calculate(number1, number2, operator);
+
+            System.out.println("Ergebnis: " + result);
+
+            String antwort = InputHelper.readString(
+                    "Weitere Berechnung durchführen? (ja/nein): ");
+
+            if (!antwort.equalsIgnoreCase("ja")) {
+                weiter = false;
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("Programm beendet.");
     }
 }
