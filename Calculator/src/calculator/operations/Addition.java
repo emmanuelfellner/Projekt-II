535533f02c9fddd305b0174d44c5d906108c3c36
iL-MaKi <maki2672@gmail.com>
2026-05-19 10:54:55 +0200

    Aufgabe #1
    Aufgabe 1 von MK erledigt

--- Calculator/src/calculator/operations/Addition.java
+++ Calculator/src/calculator/operations/Addition.java
@@ -3,6 +3,7 @@ package calculator.operations;
 public class Addition {
 
     public static double calculate(double a, double b) {
+        System.out.printf("Addition von %d und %d", a, b);
         return a + b;
     }
 }
